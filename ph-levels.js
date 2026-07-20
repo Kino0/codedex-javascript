452a8c96b3e6f8865b22d993970c87fb814bcd9d
Denis <contato.denismauricio@gmailcom>
2026-07-20 08:07:17 -0300

    feat(ph-levels): adiciona logica condicional baseada em nivel de ph

---
+++ ph-levels.js
@@ -0,0 +1,11 @@
+// Provide Ph level from 0 - 14
+const ph = 12;
+// Normal ph level is 7
+
+if (ph > 7){
+    console.log("Basic");
+} else if (ph < 7){
+    console.log("Acidic");
+} else {
+    console.log("Neutral");
+}
