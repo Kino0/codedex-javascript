1c33e268b05d73cd07a0ecc575ee440dd8c95bfb
Denis <contato.denismauricio@gmailcom>
2026-07-20 10:18:23 -0300

    feat(random-number): adicionado gerador de numero aleatorio

---
+++ random-number.js
@@ -0,0 +1,7 @@
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+// Print a random number from 0 - 9
+let randomInt = Math.floor(Math.random() * 10);
+console.log(randomInt);
