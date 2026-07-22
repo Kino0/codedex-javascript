fe5dd7005c713ed66dc71a25b815e7efdd9b243f
Denis <contato.denismauricio@gmailcom>
2026-07-22 09:17:35 -0300

    feat: adicionado logica de loop para contagem regressiva

---
+++ blast-off.js
@@ -0,0 +1,11 @@
+// Contador passa a ser negativo aqui: i - 1
+
+for (let i = 10; i >= 0; i--){
+  
+  if (i === 0){
+    console.log("To infinity and beyond! 🚀")
+    break;
+  }
+
+  console.log(i);
+}
