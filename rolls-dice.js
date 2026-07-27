0819934188fb3a3b3041749f0e7f437ed30ba96e
Denis <contato.denismauricio@gmailcom>
2026-07-27 14:03:43 -0300

    fix: remoção da estrutura de repetição if

--- rolls-dice.js
+++ rolls-dice.js
@@ -3,17 +3,17 @@ let die2 = Math.floor(Math.random() * 4) + 1;
 
 let numberOfRolls = 0;
 
-while (die1 !== die2 || die1 == die2){
-  numberOfRolls++
+while (die1 !== die2) {
   console.log("Roll 1: " + die1)
   console.log("Roll 2: " + die2)
   console.log('-'.repeat(9))
   
-  if (die1 === die2){
-    console.log("Number of rolls: " + numberOfRolls)
-    break;
-  } else {
-    die1 = Math.floor(Math.random() * 4) + 1
-    die2 = Math.floor(Math.random() * 4) + 1
-  }
+  die1 = Math.floor(Math.random() * 4) + 1;
+  die2 = Math.floor(Math.random() * 4) + 1;
+
+  numberOfRolls++
 }
+
+console.log("Roll 1: " + die1)
+console.log("Roll 2: " + die2)
+console.log("Number of rolls: " + numberOfRolls)
