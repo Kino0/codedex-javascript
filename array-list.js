6a4c48e4754b610203a586157b54514d9ec434ea
Denis <contato.denismauricio@gmailcom>
2026-07-28 20:07:12 -0300

    feat: logica para sobreposicao de elementos no array

---
+++ array-list.js
@@ -0,0 +1,8 @@
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"];
+
+console.log("Old list: " + groceryList)
+
+groceryList[2] = "🧈 Butter";
+groceryList[4] = "🧼 Laundry soap";
+
+console.log("New list: " + groceryList)
